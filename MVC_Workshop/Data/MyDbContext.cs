744e12a2702eb475b1689fb840fe25f5623687da
Enzo <enzo.biamonti@epitech.eu>
2020-03-18 16:48:57 +0100

    Init

---
+++ MVC_Workshop/Data/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Workshop.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+
+    // TODO : Initialisez votre modèle (DbSet)
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MVC_Workshop
+
+# Installation
+
+#### **Visual studio**
+Make sure to have the latest version of visual studio. It need to support .NET \
+core 3.0.
+Then choose *File - Open file - MVC_Workshop/MVC_Workshop.sln*
